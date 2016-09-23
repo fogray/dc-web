c31636b71f9ad7e847996b8371ea96e004dd053e
fogray <forget871205@126.com>
2016-09-23 17:41:56 +0800

    Create list.js

---
+++ views/services/list.js
@@ -0,0 +1,32 @@
+var webPath = 'https://dev.imaicloud.com/dc/';
+$(function(){
+	listServices();
+}
+);
+function listServices(){
+	var $wrapObj = $('#serviceList');
+	$wrapObj.html('');
+	$.ajax({
+		url: webPath + 'api/services',
+		type: 'get',
+		error: function(e, h, r) {
+			
+		},
+		success: function(data){
+			var json = eval(data), len = json.length;
+			for (var i = 0; i < len; i++) {
+				$wrapObj.append(itemDiv());
+			}
+		}
+	});
+}
+function itemDiv(){
+	var left = '<div class="col-md-2" ><input class="selector" type="checkbox"></checkbox></div>';
+	var sn = '<div class="col-md-3"></div><div class="col-md-3">';
+	var image = '<div class="col-md-3"></div><div class="col-md-3">';
+	var ct = '<div class="col-md-3"></div><div class="col-md-3">';
+	var actions = '<div class="col-md-3"></div><div class="col-md-3">';
+	var right = '<div class="col-md-10"><div class="row">'+sn+image+ct+actions+'</div></div>';
+	return '<li><div class="row">'+left+right+'</div></li>';
+	
+}
