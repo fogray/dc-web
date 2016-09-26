7934a4a0f54592fe20956ce1337dfacbff753498
fogray <forget871205@126.com>
2016-09-26 15:38:12 +0800

    Update list.js

--- views/services/list.js
+++ views/services/list.js
@@ -29,14 +29,14 @@ function itemDiv(data){
   , image = data.Spec.TaskTemplate.ContainerSpec.Image
   , replicas = data.Spec.Mode.Replicated.Replicas;
   var left = '<div class="col-sm-2" ><input class="selector" type="checkbox" name="selector"/></div>';
-  var sn = '<div class="col-sm-12">'
+  var sn = '<div class="col-md-4">'
         +'<div class="row"><div class="col-md-12">'+name+'</div></div>'
         +'<div class="row"><div class="col-md-12">'+replicas+'</div></div>'
         +'<div class="row"><div class="col-md-12" name="s_stats">RUNNING</div></div>'
         +'</div>';
-  var image = '<div class="col-md-3">'+image+'</div>';
+  var image = '<div class="col-md-4">'+image+'</div>';
   var ct = '<div class="col-md-3">'+ut+'</div>';
-  var actions = '<div class="col-md-3">'
+  var actions = '<div class="col-md-1">'
                   +'<div class="btn-group">'
                     +'<a class="btn btn-elipsedropdown-toggle" data-toggle="dropdown" aria-haspopup="true">...</a>'
                     +'<ul class="dropdown-menu">'
@@ -46,7 +46,7 @@ function itemDiv(data){
                       +'<li><a href="#">Terminate</a></li>'
                     +'</ul>'
                   +'</div></div>';
-  var right = '<div class="col-md-10"><div class="row">'+sn+image+ct+actions+'</div></div>';
+  var right = '<div class="col-sm-10"><div class="row">'+sn+image+ct+actions+'</div></div>';
   return '<li data-sid="'+s_id+'"><div class="row">'+left+right+'</div></li>';
   
 }
