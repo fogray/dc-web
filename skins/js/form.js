2df9eb75e1129bf56bd65780663c3c1a63a5a5b8
fogray <forget871205@126.com>
2016-09-27 09:50:05 +0800

    Update form.js

--- skins/js/form.js
+++ skins/js/form.js
@@ -2,11 +2,11 @@ $(function(){
   $('input[type="checkbox"].selector.selector-all').click(function(){
     if (this.checked) {
       $('input[type="checkbox"][name="selector"]').each(function(){
-        $(this).attr('checked', true);
+        $(this).attr('checked', true).checkboxradio("refresh");
       });
     } else {
       $('input[type="checkbox"][name="selector"]:checked').each(function(){
-        $(this).attr('checked', false);
+        $(this).attr('checked', false).checkboxradio("refresh");
       });
     }
   });
