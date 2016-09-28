40f0d7dff31f0bc1f3aa1ce8153dc6e0ad24180f
fogray <forget871205@126.com>
2016-09-28 11:05:51 +0800

    Update action.js

--- views/services/action.js
+++ views/services/action.js
@@ -1,18 +1,18 @@
 var sapiPath = '//dev.imaicloud.com/dc/api/services';
 var ServiceAction = (function(){
-  function create(service_conf){
+  var create = function(service_conf){
     // 弹出选择image页面，选择image后，跳转到service设置页面,设置完成后点击"Create"按钮创建service
     $.post(sapiPath+'/create', service_conf, function(text, status){
       alert(status+': '+text);
     });
   }
-  function inspect(sid){
+  var inspect = function(sid){
     $.get(sapiPath+'/'+sid+'/inspect', {serviceId:sid}, function(text, status){
       alert(status+': '+text);
     });
   }
   
-  function start(sid){
+  var start = function(sid){
     //根据service id查询出该service的所有task
     //tasks?filters={%22service%22:[%2294wkdf86cbyjgkthp3nsqjihn%22]}
     // 在task列表中检出container id，start container操作
@@ -20,18 +20,18 @@ var ServiceAction = (function(){
       alert(status+': '+text);
     });
   }
-  function stop(sid){
+  var stop = function(sid){
     $.post(sapiPath+'/'+sid+'/stop', {serviceId:sid}, function(text, status){
       alert(status+': '+text);
     });
   }
-  function redeploy(sid){
+  var redeploy = function(sid){
     return;
     $.post(sapiPath+'/'+sid+'/redeploy', {serviceId:sid}, function(text, status){
       alert(status+': '+text);
     });
   }
-  function terminate(sid){
+  var terminate = function(sid){
     $.post(sapiPath+'/'+sid, {_method:'delete', serviceId:sid}, function(text, status){
       alert(status+': '+text);
     });
