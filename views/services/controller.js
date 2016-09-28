feab98cb535516fe49c28fe819a52b992bf09ab5
fogray <forget871205@126.com>
2016-09-28 16:16:35 +0800

    Update controller.js

--- views/services/controller.js
+++ views/services/controller.js
@@ -1,5 +1,15 @@
-function ImagesListCtrl($scope){
-    $.get('http://dev.imaicloud.com/dc/api/images').success(function(data){
-      $scope.images = eval(data);
-    });
-  }
+/* All angular application controllers */
+var seagullControllers = angular.module('seagullControllers', []);
+
+/* Images controller requests beego API server to get/delete images */
+seagullControllers.controller('ImagesController', ['$scope', '$rootScope', '$routeParams', '$http',
+  function($scope, $rootScope, $routeParams, $http) {
+      // Sort table, refer to https://docs.angularjs.org/api/ng/filter/orderBy
+      $scope.predicate = '';
+      $scope.reverse = false;
+
+      /* Request beego API server to get images */
+      $http.get('http://dev.imaicloud.com/dc/api/images').success(function(data) {
+        $scope.images = data;
+      });
+}]);
