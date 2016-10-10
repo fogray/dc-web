f489dcb2ef348b1698a274520edb4f99a939d384
fogray <forget871205@126.com>
2016-10-10 10:47:36 +0800

    Update wizard.js

--- views/services/wizard.js
+++ views/services/wizard.js
@@ -1,3 +1,10 @@
 $(function(){
   $('#ifrm-service-create').attr('src', 'images.html');
 });
+
+function changeStep(step) {
+  $('.mt-element-step .step-thin . mt-step-col').removeClass('done');
+  $('.mt-element-step .step-thin . mt-step-col').removeClass('active');
+  $($('.mt-element-step .step-thin . mt-step-col')[(step-1)]).addClass('done');
+  $($('.mt-element-step .step-thin . mt-step-col')[(step-1)]).addClass('active');
+}
