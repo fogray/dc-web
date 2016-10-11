2f60a8462f7f900de6c378b413387d0760701774
fogray <forget871205@126.com>
2016-10-11 15:10:05 +0800

    Update wizard.js

--- views/services/wizard.js
+++ views/services/wizard.js
@@ -11,5 +11,5 @@ function changeStep(step) {
   $($('.mt-element-step .step-thin .mt-step-col')[(step-1)]).addClass('active');
 }
 function autoIframeHeight(){
-  $('#ifrm-service-create').style('height', $(document, $('#ifrm-service-create'))[0].body.scrollHeight);
+  $('#ifrm-service-create').css('height', $(document, $('#ifrm-service-create'))[0].body.scrollHeight);
 }
