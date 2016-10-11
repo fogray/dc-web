570e7a07c0eb3ee521cb926a11cee8b2d1d972ce
fogray <forget871205@126.com>
2016-10-11 19:23:19 +0800

    Update config.js

--- views/services/wizard/config.js
+++ views/services/wizard/config.js
@@ -1,6 +1,69 @@
+var image = null;
+$(function(){
+  image = getParam('image');
+  //根据image查询image详细信息
+  loadImageInfo();
+  
+});
+
+function loadImageInfo(){
+  $.get(DC_CONFIG.DC_API_HOST + '/image/'+image).success(function(data){
+    var config = data.Config, volumes = config.Volumes, cmd = config.Cmd, exposedPorts = config.ExposedPorts
+    , env = config.Env, labels = config.Labels, dir = config.WorkingDir, user = config.User;
+    $('#user').val(user), $('#dir').val(dir), $('#command').val(cmd.join(' '));
+    
+    if (volumes != null) {
+      var tr = '';
+      for (var key in volumes) {
+        tr += '<tr>'
+                +'<td>' + key + '<td>'
+                +'<td>' + JSON.stringify(volumes[key]) + '<td>'
+                +'<td></td>'
+                +'</tr>';
+      }
+      $('#tblVolumes tbody').append(tr);
+    }
+    
+    if (exposedPorts != null) {
+      var tr = '';
+      for (var key in exposedPorts) {
+        tr += '<tr>'
+                +'<td>' + key.split('/')[0] + '<td>'
+                +'<td>' + key.split('/')[1] + '<td>'
+                +'<td></td>'
+                +'<td></td>'
+                +'</tr>';
+      }
+      $('#tblEpPort tbody').append(tr);
+    }
+    
+    if (env != null && env.length > 0) {
+      var tr = '';
+      for (var i = 0; i < env.length; i++) {
+        tr += '<tr>'
+                +'<td>' + env[i].split('=')[0] + '</td>'
+                +'<td>' + env[i].split('=')[1] + '</td>'
+                + '</tr>';
+      }
+      $('#tblEnvs tbody').append(tr);
+    }
+    
+    if (labels != null) {
+      var tr = '';
+      for (var key in labels) {
+        tr += '<tr>'
+                +'<td>' + key + '<td>'
+                +'<td>' + labels[key] + '<td>'
+                +'</tr>';
+      }
+      $('#tblLabels tbody').append(tr);
+    }
+    
+  });
+}
 
 var configService = function(){
-  var sname = $('#serviceName').val(), image = getParam('image')
+  var sname = $('#serviceName').val()
   , stack = $('#stackList').val(), restartCondition = $('input[name="restartCondition"]:checked').val()
   , mode = $('input[name="mode"]:checked').val(), containers = $('#containers').val(), network = $('#networkList').val()
   , command = $('#command').val(), cmd_args = $('#args').val()
@@ -58,3 +121,50 @@ function getLabelFromTbl(table){
   var trs = $('tbody tr', $(table));
   if (trs.length == 0) return null;
   var labels = {};
+  for (var i = 0; i < trs.length; i++) {
+    var name = $('td', $(trs[i]))[0].innerHTML
+    , key = $('td', $(trs[i]))[1].innerHTML
+    labels[name] = key;
+  }
+  return labels;
+}
+
+function getVolumesFromTbl(table){
+  var trs = $('tbody tr', $(table));
+  if (trs.length == 0) return null;
+  var mounts = {};
+  for (var i = 0; i < trs.length; i++) {
+    var target = $('td', $(trs[i]))[0].innerHTML
+    , src = $('td', $(trs[i]))[1].innerHTML
+    , readOnly = $('td', $(trs[i]))[2].innerHTML == 'Readable' ? true : false;
+    mounts['Target'] = target, mounts['Source'] = src, mounts['ReadOnly'] = readOnly;
+  }
+  return mounts;
+}
+
+function getPortsFromTbl(table){
+  var trs = $('tbody tr', $(table));
+  if (trs.length == 0) return null;
+  var ports = [];
+  for (var i = 0; i < trs.length; i++) {
+    var c_port = $('td', $(trs[i]))[0].innerHTML
+    , protocol = $('td', $(trs[i]))[1].innerHTML
+    , published = $('td', $(trs[i]))[2].innerHTML
+    , node_port = $('td', $(trs[i]))[3].innerHTML;
+    if (c_port == '' || (published && node_port == '')) continue;
+    ports.push({Protocol: protocol, PublishedPort: c_port, TargetPort: node_port});
+  }
+  return ports;
+}
+
+function getEnvsFromTbl(table){
+  var trs = $('tbody tr', $(table));
+  if (trs.length == 0) return null;
+  var envs = [];
+  for (var i = 0; i < trs.length; i++) {
+    var env_name = $('td', $(trs[i]))[0].innerHTML
+    , env_value = $('td', $(trs[i]))[1].innerHTML;
+    envs.push(env_name+'='+env_value);
+  }
+  return envs;
+}
