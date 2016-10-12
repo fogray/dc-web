6548720c69c1f0135e59fc471df803f4d17f4b55
fogray <forget871205@126.com>
2016-10-12 09:36:38 +0800

    Update config.js

--- views/services/wizard/config.js
+++ views/services/wizard/config.js
@@ -31,7 +31,7 @@ $(function(){
               +'<td><span class="glyphicon glyphicon-trash"></span></td></tr>';
     tb.prepend(tr);
   });
-  $('table tbody tr input[name="published"]').change(function(){
+  $('table tbody tr input[name="published"]').live('change', function(){
     if ($(this).prop("checked")) {
       $('input[name="node_port"]', $(this).parents('tr')).val('dynamic');
     } else {
@@ -40,7 +40,7 @@ $(function(){
   });
   
   //删除行操作
-  $('table tbody tr .glyphicon.glyphicon-trash').click(function(){
+  $('table tbody tr .glyphicon.glyphicon-trash').live('click', function(){
     $(this).parents('tr').remove();
   });
 });
