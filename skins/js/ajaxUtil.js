0d8848d2b0347d0009bac813ff92d863b9052b24
fogray <forget871205@126.com>
2016-10-13 09:09:07 +0800

    Create ajaxUtil.js

---
+++ skins/js/ajaxUtil.js
@@ -0,0 +1,35 @@
+var AjaxTool = {
+  post: function(url, requestBody, success, error){
+    $.ajax({
+      url: url,
+      type: 'post',
+      dataType": 'json',
+      contentType: 'application/json',
+      data: JSON.stringify(requestBody),
+      error: error,
+      success: success
+    });
+  },
+  
+  get: function(url, params, success, error){
+    $.ajax({
+      url: url,
+      type: 'get',
+      dataType": 'json',
+      data: JSON.stringify(params),
+      error: error,
+      success: success
+    });
+  }
+  
+  delete: function(url, params, success, error){
+    $.ajax({
+      url: url,
+      type: 'DELETE',
+      dataType": 'json',
+      data: JSON.stringify(params),
+      error: error,
+      success: success
+    });
+  }
+};
