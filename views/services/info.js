96c6e1876caa27447407da9781ab8d6a4bfcd2b1
fogray <forget871205@126.com>
2016-10-14 09:00:22 +0800

    Update info.js

--- views/services/info.js
+++ views/services/info.js
@@ -17,7 +17,18 @@ function loadServiceInfo(){
       $('#command').html(data.Spec.TaskTemplate.ContainerSpec.Command.join(' '));
       $('#mode').html(data.Spec.Mode.hasOwnProperty('Replicated') ? 'Replicated' : 'global');
       $('#restartCondition').html(data.Spec.TaskTemplate.RestartPolicy.Condition);
-      $('#restartCondition').html(data.Spec.TaskTemplate.RestartPolicy.Condition);
+      $('#network').html(data.Spec.Networks[0].Target);
+      var ports = data.Spec.EndpointSpec.Ports;
+      if (ports != null && ports.length > 0){
+        for (var i = 0; i < ports.length; i++) {
+          var tr = '<tr><td>'+ports[i].TargetPort+'</td><td>'+ports[i].Protocol+'/'+ports[i].PublishedPort+'</td><tr>';
+          $('#tblPorts tbody').append(tr);
+        }
+      }
     }
   });
+}
+
+function loadTasksInfo(){
+  
 }
