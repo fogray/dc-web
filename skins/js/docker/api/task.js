9a474ecfb10d184ba1cbf1c57cb8afa0bb1885d7
fogray <forget871205@126.com>
2016-10-14 15:06:37 +0800

    Update task.js

--- skins/js/docker/api/task.js
+++ skins/js/docker/api/task.js
@@ -1,3 +1,22 @@
-var TaskAction = {
+var tapiPath = DC_CONFIG.DC_API_HOST + '/tasks';
+var TaskAction = (function(){
+  var list = function(params, success){
+    $.get(tapiPath, params, function(text, status){
+      if (typeof success == 'function'){
+        success(text, status);
+      }
+    });
+  }
+  var inspect = function(taskId, success){
+    $.get(tapiPath+'/'+taskId+'/inspect', {}, function(text, status){
+        if (typeof success == 'function'){
+          success(text, status);
+        }
+    });
+  }
   
-};
+  return {
+    list: list,
+    inspect: inspect
+  }
+})();
