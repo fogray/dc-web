a055eb079b91a3fac82e9e6d41ecb1bf09c14631
fogray <forget871205@126.com>
2016-10-18 16:28:07 +0800

    Update info.js

--- views/services/info.js
+++ views/services/info.js
@@ -7,7 +7,7 @@ $(function(){
     window.location.href = 'edit.html?service_id='+service_id;
   });
   $('#btnScale').click(function(){
-    var scales = NoUiSliderDom.get($('#slider-step')[0]);
+    var scales = NoUiSliderDom.getValue($('#slider-step')[0]);
     ServiceAction.scale(service_id, scales);
   });
 });
