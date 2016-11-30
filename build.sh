98815428d586fd670716f7ec5529d62e6847d82e
fogray <forget871205@126.com>
2016-11-30 09:29:15 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+ 
+#1 创建构建日志文件
+buildlog=/var/owncloud/data/fogray/files/dc/logs/build.log
+touch $buildlog
+echo "*******************start**************" >> $buildlog
+date >> $buildlog
+ 
+#2 获取已经在运行的dc的container ID
+CID=$(docker ps | grep "dc" | awk '{print $1}')
+echo "正在运行的dc容器ID$CID" >> $buildlog
+
+#3 构建image
+docker build -t dc . | tee -a $buildlog
+RESULT=$(cat $buildlog | tail -n 1)
+if [["$RESULT" != *Successfully*]];then
+#4 构建image失败，跳出脚本
+  exit -1
+fi
+
+#5 停止并删除正在运行的dc container
+if [ "$CID" != "" ];then
+  echo '>>停止正在运行的dc容器' >> $buildlog
+  docker stop $CID | tee -a $buildlog
+  echo '>>删除正在运行的dc容器' >> $buildlog
+  docker rm $CID | tee -a $buildlog
+fi
+ 
+#6 运行构建好的dc image
+#挂载jdbc.properties文件，动态配置数据源
+docker run -d -p 10060:8080  dc | tee -a $buildlog
+
+echo "*******************end**************" >> $buildlog
