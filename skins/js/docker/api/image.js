f3cebcb4923e2bf02cc5dc641f9380141ea45d45
fogray <forget871205@126.com>
2016-12-10 16:13:46 +0800

    css

--- skins/js/docker/api/image.js
+++ skins/js/docker/api/image.js
@@ -6,25 +6,27 @@ var ImagesAction = (function(){
     	if (typeof error_cal =='function'){
     		error_cal(e, h, r);
     	} else {
-        	ToastrTool.error('List images failure ', r);
+        	ToastrTool.error('List images failed ', r);
     	}
     });
   };
   
   var inspect = function(imageId, success_cal, error_cal){
     AjaxTool.get(DC_CONFIG.DC_API_IMAGES_PATH+'/'+imageId, null, function(text, status){
-        if (typeof success_cal == 'function'){
-          success_cal(text, status);
-	    } else {
-	    	if (status != 'success'){
-		        ToastrTool.error('Inspect image failure: ' + status);
-		    }
+    	if (status == 'success' && typeof text == 'object' && text.hasOwnProperty('Id')){
+    	  if (typeof success_cal == 'function'){
+	        success_cal(text, status);
+		  } else {
+		    ToastrTool.success('Inspect image success');
+	      }
+    	} else {
+		  ToastrTool.error('Inspect image failed: ' + status);
 	    }
     }, function(e, h, r){
     	if (typeof error_cal == 'function'){
           error_cal(text, status);
 	    } else {
-	    	ToastrTool.error('Inspect image failure ', r);
+	    	ToastrTool.error('Inspect image failed ', r);
 	    }
     });
   };
