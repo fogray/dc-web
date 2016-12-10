d5857f56223ef303445c4346ecf88fd966ba5a34
fogray <forget871205@126.com>
2016-12-10 15:02:04 +0800

    sc

--- skins/js/App.js
+++ skins/js/App.js
@@ -1,16 +1,3 @@
 var App = {
-	blockUI: function(obj){
-		$.blockUI({
-			message: 'loading...',
-			baseZ: 10000,
-			overlayCSS: {
-                        backgroundColor: '#555',
-                        opacity: 0.05,
-                        cursor: 'wait'
-                    }
-		});
-	},
-	unblockUI: function(obj){
-		$.unblockUI();
-	}
+
 };
